03ea10f1110517698c8e9211313f2515de93a508
nicholaschen09 <nicholas.chen243@gmail.com>
2025-05-31 11:41:15 -0400

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Header from '@/components/header';
 import Link from 'next/link';
 import Footer from '@/components/footer/footer';
 import { usePathname } from 'next/navigation';
+import Head from 'next/head';
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ['latin'],
@@ -23,9 +24,34 @@ export default function RootLayout({
   const pathname = usePathname();
   return (
     <html lang="en" className={jetbrainsMono.variable}>
+      <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5WMQFTCH');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </Head>
       <body
         className={`${jetbrainsMono.className} bg-[#1a1a1a] min-h-screen antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5WMQFTCH"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {/* Easter egg button */}
         {pathname !== '/draw' && (
           <Link

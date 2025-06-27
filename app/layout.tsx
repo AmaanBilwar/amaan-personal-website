6756c9d3cb3263d4bcf21879ae175395de349037
nicholaschen09 <nicholas.chen243@gmail.com>
2025-06-27 15:28:46 -0400

    fix google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,33 +35,26 @@ export default function RootLayout({
     <LanguageProvider>
       <html lang="en" className={`${jetbrainsMono.variable} ${minecraft.variable} font-minecraft`}>
         <Head>
-          {/* Google Tag Manager */}
+          {/* Google Analytics Script */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-J6HJSY8DQ4"
+          ></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-5WMQFTCH');
-              `,
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-J6HJSY8DQ4');
+                `,
             }}
           />
-          {/* End Google Tag Manager */}
         </Head>
         <body
           className={`bg-[#1a1a1a] min-h-screen antialiased font-minecraft`}
         >
-          {/* Google Tag Manager (noscript) */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-5WMQFTCH"
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
-          {/* End Google Tag Manager (noscript) */}
+          
           {/* Navigation buttons */}
           <NavigationButtons />
           <LanguageToggle />

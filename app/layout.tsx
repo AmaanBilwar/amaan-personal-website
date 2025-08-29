28a9b2ccb1b5d6d50dda1a9276828778439437e8
nicholaschen09 <nicholas.chen243@gmail.com>
2025-08-29 10:46:56 -0400

    fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
       <html lang="en" className={`${jetbrainsMono.variable} ${minecraft.variable} font-minecraft`}>
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <link rel="icon" href="/favicon.ico" />
         </head>
 
         <body

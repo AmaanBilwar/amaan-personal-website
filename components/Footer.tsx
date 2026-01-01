8e947d6d53f2a1a2ed93189803bc0ee394c115bd
nicholaschen09 <nicholas.chen243@gmail.com>
2026-01-01 09:28:29 -0500

    mail

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,8 +19,8 @@ export default function Footer({ className = 'mt-20' }: FooterProps) {
           aria-label="Email"
         >
           <svg
-            width="20"
-            height="20"
+            width="22"
+            height="22"
             viewBox="0 0 24 24"
             fill="currentColor"
             xmlns="http://www.w3.org/2000/svg"

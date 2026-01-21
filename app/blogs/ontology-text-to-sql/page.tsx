02194aaa0047560fe010fd3eb893efc10aa58414
nicholaschen09 <nicholas.chen243@gmail.com>
2026-01-20 23:16:16 -0500

    fix

--- app/blogs/ontology-text-to-sql/page.tsx
+++ app/blogs/ontology-text-to-sql/page.tsx
@@ -29,7 +29,7 @@ export default function OntologyTextToSqlBlog() {
   return (
     <main className="min-h-screen bg-[#1a1a1a] text-stone-300 pb-12 px-4 md:px-8">
       <div className="max-w-6xl mx-auto pt-12 flex gap-8 items-start justify-center">
-        <TableOfContents sections={sections} />
+        <TableOfContents sections={sections} title={t('blog.contents')} />
         <article className="w-full lg:max-w-lg">
           {/* Back link */}
           <Link

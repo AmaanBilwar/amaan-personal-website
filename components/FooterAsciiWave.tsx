3b5db30ce742a2c3322f346fc7b41a54a47c22b8
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-07-07 18:01:20 +0530

    ascii work (incomplete)

---
+++ components/FooterAsciiWave.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Rune } from 'rune-ascii';
+
+export default function FooterAsciiWave() {
+  return (
+    <div aria-hidden className="opacity-70">
+      <Rune src="" />
+    </div>
+  );
+}

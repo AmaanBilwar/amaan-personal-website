027681ef5348802cf1684b8ff011bd3024ee0c62
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-07-19 09:57:52 +0530

    more blog changes

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -2,10 +2,12 @@ import NormalKeyboard from "@/components/blog-scenes/NormalKeyboard";
 import HomeRowKeyboard from "@/components/blog-scenes/HomeRowKeyboard";
 import LowerLayerKeyboard from "@/components/blog-scenes/LowerLayerKeyboard";
 import ArrowKeysKeyboard from "@/components/blog-scenes/ArrowKeysKeyboard";
+import Keyboard from "@/components/blog-scenes/Keyboard";
 
 export const mdxComponents = {
   HomeRowKeyboard,
   NormalKeyboard,
   LowerLayerKeyboard,
   ArrowKeysKeyboard,
+  Keyboard,
 };
